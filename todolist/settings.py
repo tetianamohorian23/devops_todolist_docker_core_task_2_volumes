4ed7faadc210dc3c8c862468d2f47b1a9e68db18
Tetiana Mohorian <tetiana.mohorian@student.tuke.sk>
2026-08-19 22:29:35 +0300

    Update settings.py

--- todolist/settings.py
+++ todolist/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
             'USER': 'app_user',
             'PASSWORD': '1234',
             'HOST': '172.17.0.2', 
-            'PORT': '3306',
+            'PORT': '',
         }
     }
 
